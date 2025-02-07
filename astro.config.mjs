fb1d945977819ac570fbf43504b4fe10d9e8c5f3
NLW-paulm <pmc@llgc.org.uk>
2025-02-07 12:07:45 +0000

    Updating base dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  base: '/nlw-data', // Replace 'directory' with your actual subdirectory name
+  base: '/data-bardic', // Replace 'directory' with your actual subdirectory name
   outDir: './dist', // This is where your built files will go
   build: {
     assets: 'assets'
